187ae62374a6c5b1822e7c212cf3685b7dc4b920
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-10-30 09:24:56 +0600

    Update

--- src/Pages/Home/Living/Living.js
+++ src/Pages/Home/Living/Living.js
@@ -9,7 +9,7 @@ const Living = () => {
                 <div className="grid grid-cols-2 grid-10">
                     <div className="living-col">
                         <h2>Comfortable Stay</h2>
-                        <p>Sarah Resort features 22 Premium King Rooms, 18 Superior King. 30 Superior Twin, 6 Deluxe King/Twin, and 1 Presidential Villa (3 B/R). All guests’ rooms & suites are designed with the latest state of the art technology and furnished in modern contemporary style, all to give the utmost comfortable work and sleep environment.</p>
+                        <p>Tourverse features 180 Premium resorts around the country, 55 Superior King. 40 Superior Twin, 10 Deluxe King/Twin, and 1 Presidential Villa (3 B/R). All guests’ rooms & suites are designed with the latest state of the art technology and furnished in modern contemporary style, all to give the utmost comfortable work and sleep environment.</p>
                     </div>
                     <div className="living-corousel">
                         <SimpleSlider></SimpleSlider>
