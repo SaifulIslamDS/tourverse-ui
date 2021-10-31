7115f16c9aa358320be5a4e7256ee34debeea99c
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-10-31 07:05:43 +0600

    Update

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -11,6 +11,7 @@ const Login = () => {
             <div>
                 <button onClick={handleGoogleLogin} className="auth-btn">Sign-in with Google</button>
                 <button onClick={handleSignOut} className="auth-btn">Logout</button>
+
                 <h2 className="username">{user?.displayName}</h2>
                 <div className="userphoto"><img src={user.photoURL} alt="" /></div>
                 <h3 className="useremail">{user?.email}</h3>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuths from '../../Hooks/useAuths';
 import './Header.css';
 import logo from '../../Images/tourverse.png';
 
 const Header = () => {
+    const {user, handleSignOut} = useAuths();
     return (
         <header id="header">
             <div className="container mx-auto flex">
@@ -14,7 +16,15 @@ const Header = () => {
                     <div className="nav">
                         <Link to="/">Home</Link>
                         <Link to="/placeorder">Place Order</Link>
-                        <Link to="/login">Login</Link>
+                        <span>{user.email}</span>
+                        { 
+                            (user.email) ? (
+                            <Link to="/login"><button onClick={handleSignOut}>  Logout
+                            </button></Link>) 
+                            
+                            : (
+                                <Link to="/login">Login</Link>)
+                        }
                     </div>
                 </div>
             </div>
