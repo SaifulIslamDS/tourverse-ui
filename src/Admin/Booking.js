f15ad00caa563e5891fcd840e41c176ff2540771
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-03 16:47:21 +0600

    Update

--- src/Admin/Booking.js
+++ src/Admin/Booking.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import useAuths from '../Hooks/useAuths';
 import './Booking.css';
 
 const Booking = () => {
-    const {user} =  useAuths();
-    const handleConfirmBtn = (e) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-        e.preventDefault();
-    } 
+
+    const {user} =  useAuths();
+
+    const onConfirm = data => {
+        const url = `http://localhost:7000/bookings`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result);
+            if (result.insertedId) {
+                alert('Order processed Successfully');
+            }
+        });
+    };
 
     return (
         <section id="booking">
             <h2 className="text-5xl">Book the resort</h2>
             
-            <form id="booking-form">
-                <input type="text" placeholder="Your name" value={user.displayName}/>
-                <input type="email" placeholder="Your email" value={user.email}/>
-                <input type="submit" onclick={handleConfirmBtn} value="Click to confirm"/>
+            <form id="booking-form" onSubmit={handleSubmit(onConfirm)}>
+                
+                <input defaultValue={user.displayName} {...register("name")} />
+                <input defaultValue={user.email} {...register("email", { required: true })} />
+                
+                <input type="submit"  value="Click to confirm"/>
             </form>
 
         </section>
