88124e65865f6067a81505637d1c18fb0eab7531
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-08 00:12:01 +0600

    Update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,21 +6,5 @@ const firebaseConfig = {
     messagingSenderId: "774411250868",
     appId: "1:774411250868:web:b88c618455f9af7bf0bbe1"
 };
+
 export default firebaseConfig;
-
-
-// apiKey: process.env.API_KEY,
-// authDomain: process.env.AUTH_DOMAIN,
-// projectId: process.env.PROJECT_ID,
-// storageBucket: process.env.STORAGE_BUCKET,
-// messagingSenderId: process.env.MESSAGING_SENDER_ID,
-// appId: process.env.APP_ID
-
-
-
-// apiKey: "AIzaSyBvhR62Kw47p9osgYxkI37ij6RB854MeS4",
-// authDomain: "tourverse-ss.firebaseapp.com",
-// projectId: "tourverse-ss",
-// storageBucket: "tourverse-ss.appspot.com",
-// messagingSenderId: "774411250868",
-// appId: "1:774411250868:web:b88c618455f9af7bf0bbe1"
